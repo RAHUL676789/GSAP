1d64c3aae61d373b2b18cf8dee567636781f4e6d
=Rahul lodhi <146419777+RAHUL676789@users.noreply.github.com>
2025-05-26 11:34:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+let tl = gsap.timeline();
+
+tl.from("h1",{
+    x:-40,
+    duration:0.7,
+    opacity:0,
+    delay:1,
+
+})
+tl.from("h4",{
+    y:-40,
+    duration:0.5,
+    opacity:0,
+    stagger:0.3
+
+})
+tl.from("#name",{
+    opacity:0,
+    duration:1,
+    y:-20
+})
+tl.from("#iind",{
+    opacity:0,
+    duration:0.5,
+    y:30,
+
+})
+tl.from(".pic",{
+    opacity:0,
+    duration:1,
+    scale:0,
+    rotate:-27
+    // x:60
+
+})
+
+
+
+gsap.from(".Skills h2", {
+  x: 100,
+  duration: 1,
+  stagger: 1,
+  opacity: 0,
+  delay: 1,
+  scrollTrigger: {
+    trigger: ".Skills h2",
+    scroller: "body",     // ✅ correct spelling
+    scrub: 2,
+    start: "top 80%",
+    end: "top 20%",
+    markers: true   ,
+     toggleActions: "play none none reverse"      // ✅ also note: it's "markers" (plural)
+  }
+});
